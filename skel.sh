67abb34ca56631434c4ebbbf92f346b5f74cd695
shelldorado <shelldorado@proton.me>
2022-09-28 10:32:33 +0200

    add

--- skel.sh
+++ skel.sh
@@ -12,7 +12,7 @@ declare -A meta_=() && meta_=(
 
 ## CONFIG
 declare -A conf_=() && conf_=(
-    [pwd]="${PWD}"
+    [ts]="$( date +%s )"
 )                                                                               &&
 
 ## PATH
