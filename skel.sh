6ee70b8b8f53f9a2bc24075550c3607d024ca102
shelldorado <shelldorado@proton.me>
2022-09-28 21:09:54 +0200

    add source link

--- skel.sh
+++ skel.sh
@@ -5,6 +5,7 @@ declare -A meta_=() && meta_=(
     [info]="bash4+ skelleton"
     [version]="0.1"
     [date]="2022-09-28"
+    [source]="https://github.com/shelldorado/skel-sh.git"
     [license]="GNU GPLv3+ (see https://www.gnu.org/licenses/gpl-3.0.txt)"
     [author]="shelldorado"
     [usage]="COMMAND [OPTION]"
@@ -16,7 +17,7 @@ declare -A conf_=() && conf_=(
     [ts]="$( date +%s )"
 )                                                                               &&
 
-## PATH
+## DIR
 declare -A dir_=() && dir_=(
     [pwd]="${PWD}"
 )                                                                               &&
@@ -30,9 +31,10 @@ declare -A com_=() && com_=(
 com_help() {
     declare -a out__=() && out__=(
         "# ${meta_[name]}" "${meta_[info]}"                             $'\n'
-        "## USAGE"         "  ${0} ${meta_[usage]}"                     $'\n'
         "## VERSION"       "  ${meta_[version]} (${meta_[date]})"       $'\n'
         "## LICENSE"       "${meta_[license]}"                          $'\n'
+        "## SOURCE"        "${meta_[source]}"                           $'\n'
+        "## USAGE"         "  ${0} ${meta_[usage]}"                     $'\n'
         "## COMMANDS" ''
     )                                                                           &&
     while IFS= read -r line ; do
